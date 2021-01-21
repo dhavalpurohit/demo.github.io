2def4127a338d16d0627a7b81d551d1fad1c4a73
Dhaval Purohit <36307045+dhavalpurohit@users.noreply.github.com>
2021-01-21 16:47:02 +0530

    Update demo.js

--- src/assets/demo.js
+++ src/assets/demo.js
@@ -1,19 +1,4 @@
-import React, { Component } from 'react'
-import $ from 'jquery';
-
-class Custome extends Component{
-	componentDidMount() {
-
-		$('#adddemo').click(function() {
+$('#adddemo').click(function() {
 			alert("show alert")
 			console.log("click add demo button");
 		});
-	}
-	render() {
-		return (
-			<></>
-		);
-	}
-}
-
-export default Custome;
